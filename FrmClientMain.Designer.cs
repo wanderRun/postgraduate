8c96da8d0a42af168b191ed86dcbc2f63ddc4a81
wanderRun <bbyjbb@sohu.com>
2015-03-04 01:17:11 +0800

    聊天室客户端源代码

---
+++ FrmClientMain.Designer.cs
@@ -0,0 +1,259 @@
+﻿namespace Client
+{
+    partial class FrmClientMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lbUserList = new System.Windows.Forms.ListBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btLogin = new System.Windows.Forms.Button();
+            this.tbUserName = new System.Windows.Forms.TextBox();
+            this.tbServer = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbServerPort = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btSend = new System.Windows.Forms.Button();
+            this.cbPrivChat = new System.Windows.Forms.CheckBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.rbSendMsg = new System.Windows.Forms.RichTextBox();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.rbChatContent = new System.Windows.Forms.RichTextBox();
+            this.btQuit = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lbUserList);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(164, 287);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "在线用户列表";
+            // 
+            // lbUserList
+            // 
+            this.lbUserList.FormattingEnabled = true;
+            this.lbUserList.ItemHeight = 12;
+            this.lbUserList.Location = new System.Drawing.Point(6, 20);
+            this.lbUserList.Name = "lbUserList";
+            this.lbUserList.Size = new System.Drawing.Size(152, 256);
+            this.lbUserList.TabIndex = 0;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.tbServer);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.tbServerPort);
+            this.groupBox2.Controls.Add(this.btLogin);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.tbUserName);
+            this.groupBox2.Location = new System.Drawing.Point(183, 13);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(537, 50);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "设置登录服务器";
+            // 
+            // btLogin
+            // 
+            this.btLogin.Location = new System.Drawing.Point(453, 12);
+            this.btLogin.Name = "btLogin";
+            this.btLogin.Size = new System.Drawing.Size(75, 23);
+            this.btLogin.TabIndex = 0;
+            this.btLogin.Text = "登录";
+            this.btLogin.UseVisualStyleBackColor = true;
+            this.btLogin.Click += new System.EventHandler(this.btLogin_Click);
+            // 
+            // tbUserName
+            // 
+            this.tbUserName.Location = new System.Drawing.Point(347, 14);
+            this.tbUserName.Name = "tbUserName";
+            this.tbUserName.Size = new System.Drawing.Size(100, 21);
+            this.tbUserName.TabIndex = 1;
+            // 
+            // tbServer
+            // 
+            this.tbServer.Location = new System.Drawing.Point(53, 14);
+            this.tbServer.Name = "tbServer";
+            this.tbServer.Size = new System.Drawing.Size(100, 21);
+            this.tbServer.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(300, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "用户名";
+            // 
+            // tbServerPort
+            // 
+            this.tbServerPort.Location = new System.Drawing.Point(194, 14);
+            this.tbServerPort.Name = "tbServerPort";
+            this.tbServerPort.Size = new System.Drawing.Size(100, 21);
+            this.tbServerPort.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(159, 17);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 12);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "端口";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 17);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 12);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "服务器";
+            // 
+            // btSend
+            // 
+            this.btSend.Location = new System.Drawing.Point(636, 232);
+            this.btSend.Name = "btSend";
+            this.btSend.Size = new System.Drawing.Size(75, 23);
+            this.btSend.TabIndex = 2;
+            this.btSend.Text = "发送";
+            this.btSend.UseVisualStyleBackColor = true;
+            this.btSend.Click += new System.EventHandler(this.btSend_Click);
+            // 
+            // cbPrivChat
+            // 
+            this.cbPrivChat.AutoSize = true;
+            this.cbPrivChat.Location = new System.Drawing.Point(636, 210);
+            this.cbPrivChat.Name = "cbPrivChat";
+            this.cbPrivChat.Size = new System.Drawing.Size(48, 16);
+            this.cbPrivChat.TabIndex = 3;
+            this.cbPrivChat.Text = "私聊";
+            this.cbPrivChat.UseVisualStyleBackColor = true;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.rbSendMsg);
+            this.groupBox3.Location = new System.Drawing.Point(183, 199);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(446, 100);
+            this.groupBox3.TabIndex = 4;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "发送信息";
+            // 
+            // rbSendMsg
+            // 
+            this.rbSendMsg.Location = new System.Drawing.Point(6, 20);
+            this.rbSendMsg.Name = "rbSendMsg";
+            this.rbSendMsg.Size = new System.Drawing.Size(434, 74);
+            this.rbSendMsg.TabIndex = 0;
+            this.rbSendMsg.Text = "";
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.rbChatContent);
+            this.groupBox4.Location = new System.Drawing.Point(183, 69);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(537, 124);
+            this.groupBox4.TabIndex = 5;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "聊天内容";
+            // 
+            // rbChatContent
+            // 
+            this.rbChatContent.Location = new System.Drawing.Point(6, 20);
+            this.rbChatContent.Name = "rbChatContent";
+            this.rbChatContent.Size = new System.Drawing.Size(525, 98);
+            this.rbChatContent.TabIndex = 0;
+            this.rbChatContent.Text = "";
+            // 
+            // btQuit
+            // 
+            this.btQuit.Location = new System.Drawing.Point(636, 262);
+            this.btQuit.Name = "btQuit";
+            this.btQuit.Size = new System.Drawing.Size(75, 23);
+            this.btQuit.TabIndex = 6;
+            this.btQuit.Text = "离开聊天";
+            this.btQuit.UseVisualStyleBackColor = true;
+            this.btQuit.Click += new System.EventHandler(this.btQuit_Click);
+            // 
+            // FrmClientMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(732, 311);
+            this.Controls.Add(this.btQuit);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.cbPrivChat);
+            this.Controls.Add(this.btSend);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "FrmClientMain";
+            this.Text = "聊天室客户端";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.FrmClientMain_FormClosing);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox4.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ListBox lbUserList;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btLogin;
+        private System.Windows.Forms.TextBox tbUserName;
+        private System.Windows.Forms.TextBox tbServer;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbServerPort;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btSend;
+        private System.Windows.Forms.CheckBox cbPrivChat;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.RichTextBox rbSendMsg;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.RichTextBox rbChatContent;
+        private System.Windows.Forms.Button btQuit;
+    }
+}
+
---
+++ FrmClientMain.cs
@@ -0,0 +1,261 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+// 引入Sockets和线程的命名空间
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace Client
+{
+    public partial class FrmClientMain : Form
+    {
+        // 与服务器的连接
+        private TcpClient tcpClient;
+
+        // 与服务器交流的流通道
+        private NetworkStream nsStream;
+
+        // 客户端的两种状态
+        private static string CLOSED = "closed";
+        private static string CONNECTED = "connected";
+        private string clientState = CLOSED;
+
+        private bool stopFlag;
+
+        public FrmClientMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btLogin_Click(object sender, EventArgs e)
+        {
+            if(clientState == CONNECTED)
+            {
+                return;
+            }
+
+            if(this.tbUserName.Text.Length == 0)
+            {
+                MessageBox.Show("请输入您的用户名！", "操作提示", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                this.tbUserName.Focus();
+                return;
+            }
+            try
+            {
+                // 创建一个客户端套接字，它是Login的一个公共属性，
+                // 将被传递给FrmClientMain窗体
+                tcpClient = new TcpClient();
+                // 向指定的ip地址的服务器发出连接请求
+                tcpClient.Connect(IPAddress.Parse(tbServer.Text), Int32.Parse(tbServerPort.Text));
+                // 获得与服务器数据交互的流通道(NetworkStream)
+                nsStream = tcpClient.GetStream();
+
+                // 启动一个新的线程，执行方法this.ServerResponse()，
+                // 以便来响应从服务器发回的信息
+                Thread newThread = new Thread(new ThreadStart(this.ServerResponse));
+                newThread.Start();
+
+                // 向服务器发送CONNECT请求命令，
+                // 此命令的格式与服务器端的定义格式一致，
+                // 命令格式为：命令标志符（CONNECT）|发送者的用户名|
+                string cmd = "CONNECT|" + this.tbUserName.Text + "|";
+                // 将字符串转化为字符串数组
+                Byte[] bytes = System.Text.Encoding.Default.GetBytes(cmd.ToCharArray());
+                nsStream.Write(bytes, 0, bytes.Length);
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        // 用于接收从服务器发回的信息
+        // 根据不同的命令，执行相应的操作
+        private void ServerResponse()
+        {
+            ListBox.CheckForIllegalCrossThreadCalls = false;
+            // 定义一个byte数组，用于接收从服务器端发送来的数据，
+            // 每次所能接收的数据包的最大长度为1024个字节
+            byte[] buff = new byte[1024];
+            string msg;
+            int len;
+            try
+            {
+                if(!nsStream.CanRead)
+                {
+                    return;
+                }
+
+                stopFlag = false;
+                while(!stopFlag)
+                {
+                    // 从流中得到数据，并存入buff字符数组中
+                    len = nsStream.Read(buff, 0, buff.Length);
+                    if(len < 1)
+                    {
+                        Thread.Sleep(200);
+                        continue;
+                    }
+
+                    // 将字符数组转化为字符串
+                    msg = System.Text.Encoding.Default.GetString(buff, 0, len);
+                    msg.Trim();
+
+                    string[] acceptFromServer = msg.Split(new char[] { '|' });
+                    // acceptFromSErver[0]中保存了命令标识符（LIST或JOIN或QUIT）
+
+                    if (acceptFromServer[0].ToUpper() == "OK")
+                    {
+                        // 处理响应
+                        Invoke(new addHandler(add), rbChatContent, "命令执行成功");
+                    }
+                    else if (acceptFromServer[0].ToUpper() == "ERROR")
+                    {
+                        // 命令执行错误
+                        Invoke(new addHandler(add), rbChatContent, "命令执行错误：" + acceptFromServer[1]);
+                    }
+                    else if (acceptFromServer[0] == "LIST")
+                    {
+                        // 此时从服务器返回的消息格式：
+                        // 命令标志符（LIST）|用户名1|用户名2|...（所有在线用户名）|
+                        Invoke(new addHandler(add), rbChatContent, "获得用户列表");
+
+                        // add("获得用户列表");
+                        // 更新在线用户列表
+                        lbUserList.Items.Clear();
+                        for(int i = 1; i < acceptFromServer.Length - 1; ++i)
+                        {
+                            lbUserList.Items.Add(acceptFromServer[i].Trim());
+                        }
+                    }
+                    else if(acceptFromServer[0] == "JOIN")
+                    {
+                        // 此时从服务器返回的消息格式：
+                        // 命令标识符（JOIN）|刚刚登入的用户名|
+                        Invoke(new addHandler(add), rbChatContent, acceptFromServer[1] + " 已经进入聊天室");
+                        this.lbUserList.Items.Add(acceptFromServer[1]);
+                        if(this.tbUserName.Text == acceptFromServer[1])
+                        {
+                            this.clientState = CONNECTED;
+                        }
+                    }
+                    else if(acceptFromServer[0] == "QUIT")
+                    {
+                        if(this.lbUserList.Items.IndexOf(acceptFromServer[1]) > -1)
+                        {
+                            this.lbUserList.Items.Remove(acceptFromServer[1]);
+                        }
+                        Invoke(new addHandler(add), rbChatContent, "用户：" + acceptFromServer[1] + " 离开聊天室");
+                    }
+                    else
+                    {
+                        // 如果从服务器返回的其他消息格式
+                        // 则在ListBox控件中直接显示
+                        Invoke(new addHandler(add), rbChatContent, msg);
+                    }
+                }
+                tcpClient.Close();
+            }
+            catch
+            {
+            }
+        }
+
+        delegate void addHandler(RichTextBox rb, string msg);
+
+        /// <summary>
+        /// 添加信息到聊天窗体
+        /// </summary>
+        /// <param name="msg">信息</param>
+        private void add(RichTextBox rb, string msg)
+        {
+
+            rb.SelectedText = msg + "\n";
+        }
+
+        delegate void AddUserHandler(ListBox lb, string user);
+
+        private void AddUser(ListBox lb, string user)
+        {
+
+        }
+
+        /// <summary>
+        /// 如果不是私聊，将CHAT命令发送给服务器，否则（为私聊），将PRIV命令发送给服务器，
+        /// 命令格式一定要与服务器商的命令格式一致
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btSend_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!this.cbPrivChat.Checked)
+                {
+                    // 此时命令的格式是：
+                    // 命令标志符（CHAT）|发送者的用户名：发送内容|
+                    string message = "CHAT|" + this.tbUserName.Text + ":" + rbSendMsg.Text + "|";
+                    rbSendMsg.Text = "";
+                    rbSendMsg.Focus();
+                    // 将字符串转化为字符数组
+                    Byte[] bytes = System.Text.Encoding.Default.GetBytes(message.ToCharArray());
+                    nsStream.Write(bytes, 0, bytes.Length);
+                }
+                else
+                {
+                    if(lbUserList.SelectedIndex == -1)
+                    {
+                        MessageBox.Show("请在列表中选择一个用户", "提示信息", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                        return;
+                    }
+                    string receiver = lbUserList.SelectedItem.ToString();
+                    // 消息的格式是：
+                    // 命令标识符（PRIV）|发送者的用户名|接收者的用户名|发送内容|
+                    string message = "PRIV|" + this.tbUserName.Text + "|" + receiver + "|" + rbSendMsg.Text + "|";
+                    rbSendMsg.Text = "";
+                    rbSendMsg.Focus();
+                    // 将字符串转化为字符数组
+                    byte[] bytes = System.Text.Encoding.ASCII.GetBytes(message.ToCharArray());
+                    nsStream.Write(bytes, 0, bytes.Length);
+                }
+            }
+            catch
+            {
+                this.rbChatContent.AppendText("网络发生错误或者服务器已经退出！");
+            }
+        }
+
+        /// <summary>
+        /// 离开聊天
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btQuit_Click(object sender, EventArgs e)
+        {
+            if(clientState == CONNECTED)
+            {
+                string message = "EXIT|" + this.tbUserName.Text + "|";
+                // 将字符串转化为字符数组
+                Byte[] bytes = System.Text.Encoding.Default.GetBytes(message.ToCharArray());
+                nsStream.Write(bytes, 0, bytes.Length);
+
+                this.clientState = CLOSED;
+                this.stopFlag = true;
+                this.lbUserList.Items.Clear();
+            }
+        }
+
+        private void FrmClientMain_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            btQuit_Click(sender, e);
+        }
+    }
+}
