a59fe0984613f670d361db2b0207fa11a0081cdd
wanderRun <bbyjbb@sohu.com>
2015-04-17 19:36:06 +0800

    excel操作

--- ExcelManager.cs
+++ ExcelManager.cs
@@ -1,7 +1,8 @@
 ﻿using System;
-using Excel;
-using Office;
-using AddInDesignerObjects;
+using Microsoft.Office.Interop.Excel;
+using System.Runtime.InteropServices;
+using Microsoft.Win32;
+using System.IO;
 
 namespace Server
 {
@@ -9,15 +10,44 @@ namespace Server
     {
         private Application excel = new Application();
         
-        private bool IsExcelInstalled()
+        private int ExcelInstalled()
         {
-            Type type = Type.GetTypeFromProgID("Excel.Application");
-            return type != null;
+            int ret = 0;
+            RegistryKey rk = Registry.LocalMachine;
+            RegistryKey rkOffice = rk.OpenSubKey(@"SOFTWARE\Microsoft\Office\11.0\Common\InstallRoot\");
+            RegistryKey rkWps = rk.OpenSubKey(@"SOFTWARE\Kingsoft\Office\6.0\common\");
+            string file = null;
+            if(rkOffice != null)
+            {
+                file = rkOffice.GetValue("Path").ToString();
+                if(File.Exists(file + "Excel.exe"))
+                {
+                    ret += 1;
+                }
+            }
+            return ret;
         }
 
-        public void LoadExcel()
+        public void LoadExcel(string name)
         {
-
+            try
+            {
+                Workbook book = excel.Workbooks.Open(name);
+                Worksheet sheet = book.Worksheets.Item[1];
+                int count = book.Worksheets.Count;
+                sheet.Name = "wang";
+                sheet.Visible = XlSheetVisibility.xlSheetVisible;
+                excel.Quit();
+                Marshal.ReleaseComObject(excel);
+                Marshal.ReleaseComObject(book);
+                Marshal.ReleaseComObject(sheet);
+                GC.Collect();
+                Console.WriteLine("hell");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("{0}", ex.Message);
+            }
         }
 
         public void LoadWPSExcel(string name)
@@ -25,14 +55,15 @@ namespace Server
             try
             {
                 Workbook book = excel.Workbooks.Open(name);
-                Worksheet sheet = book.Worksheets.Item[0];
+                Worksheet sheet = book.Worksheets.Item[1];
+                int count = book.Worksheets.Count;
                 sheet.Name = "wang";
                 sheet.Visible = XlSheetVisibility.xlSheetVisible;
-
-                //Marshal.ReleaseComObject(excel);
-                //Marshal.ReleaseComObject(book);
-                //Marshal.ReleaseComObject(sheet);
-                //GC.Collect();
+                excel.Quit();
+                Marshal.ReleaseComObject(excel);
+                Marshal.ReleaseComObject(book);
+                Marshal.ReleaseComObject(sheet);
+                GC.Collect();
                 Console.WriteLine("hell");
             }
             catch(Exception ex)
